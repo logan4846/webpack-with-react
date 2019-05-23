293b8f3a1212548cd8100fe5165ed25e9fd311a3
liuxx <xiangxiang.liu@weimob.com>
2019-05-23 15:21:38 +0800

    update

--- src/scripts/Login.js
+++ src/scripts/Login.js
@@ -49,6 +49,28 @@ class Login extends Component {
         // this.testPromise();
         // this.testGenerator();
         // this.testAsync();
+        //给一个对象提供for of消费
+        Object.prototype[Symbol.iterator] = function(){
+            let keys = Object.keys(this);
+            let length = keys.length;
+            let currentIndex = 0;
+            return {
+                next: () =>{
+                    let ob = {
+                        value: currentIndex <= length - 1 ? this[keys[currentIndex]] : "nomore",
+                        done: currentIndex > length -1
+                    };
+                    currentIndex++;
+                    return ob;
+                }
+            };
+        }
+        let testOb = {name:"ahaa",age:23,address:"中国重庆"};
+
+        for(let i of testOb){
+            console.log(i);
+        }
+        console.log("End");
     }
     mockPromise() {
         return new Promise(function (resolve, reject) {
@@ -114,24 +136,232 @@ class Login extends Component {
     }
 
     normalTest(){
-        this.state.name = 1;
-        this.setState((state, props) => ({
-            count: state.count + 1
-        }));
-        this.setState((state, props) => ({
-            count: state.count + 1
-        }));
+        let element = this.testE;
+        var start
+
+        function step(timestamp) {
+            console.log(timestamp);
+            if (!start) start= timestamp;
+            var progress = timestamp - start;
+            element.style.left = Math.min(progress / 10, 200) + 'px';
+            console.log( Math.min(progress / 10, 200) + 'px');
+            if (progress < 2000) {
+                window.requestAnimationFrame(step);
+            }
+        }
+
+        window.requestAnimationFrame(step);
     }
 
     componentWillUnmount() {
         clearInterval(this.interval);
     }
 
+    // 冒泡排序
+    bubbleSort(arr){
+        for(let i = 0;i < arr.length;i++){
+            for(let j = 0 ;j < arr.length-i-1;j++){
+                if(arr[j+1] < arr[j]){
+                    let temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    //选择排序
+    selectSort(arr){
+        for(let i = 0;i < arr.length;i++){
+            let minIndex = i;
+            for(let j = i+1;j< arr.length;j++){
+                if(arr[j] < arr[i]){
+                    minIndex = j;
+                }
+            }
+            console.log(`当前最小数：`,minIndex);
+            let temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+        console.log(arr);
+    }
+
+    //插入排序
+    insertSort(arr){
+        for(let i = 1;i < arr.length;i ++){
+            let temp = arr[i];
+            while(i >= 1 && arr[i-1] < temp){
+                arr[i] = arr[i - 1];
+                i--;
+            }
+            arr[i] = temp;
+        }
+        console.log(arr);
+    }
+
+    //快速排序
+    fastSort(arr){
+        if(arr.length <= 1) return arr;
+        let middleValue = arr[Math.floor(arr.length/2)];
+        let left = [];
+        let right = [];
+        for(let i = 0 ;i < arr.length ;i ++){
+            if(arr[i] < middleValue) left.push(arr[i]);
+            if(arr[i] > middleValue) right.push(arr[i]);
+        }
+        let result = [].concat(this.fastSort(left),[middleValue],this.fastSort(right));
+        console.log(result);
+        return result;
+    }
+
+    //希尔排序  数组，增量
+    shellSort(arr){
+        //[2,1,6,5,8,3,7,9,4]
+        let length = arr.length;
+        let incre = length;
+        while(incre > 1){
+            incre = Math.floor(incre/2);
+            for(let i = 0;i < incre;i++){
+                for(let j = i + incre;j<length;j+=incre){
+                    let temp = arr[j];
+                    while( j >= (i+incre) && arr[j-incre] > temp){
+                        arr[j] = arr[j-incre];
+                        j-=incre;
+                    }
+                    arr[j] = temp;
+                }
+            }
+        }
+        console.log(arr);
+    }
+
+    //归并排序
+    mergeSort(arr){
+        function mergeMain(arr1,first,last,temp){
+            if(first < last){
+                let middle = Math.floor((first + last)/2);
+                //分
+                mergeMain(arr1,first,middle,temp);
+                mergeMain(arr1,middle+1,last,temp);
+                //治
+                mergeArr(arr1,first,middle,last,temp);
+            }
+        }
+
+        function mergeArr(arr1,first,middle,end,temp){
+            let i = first;
+            let j = middle + 1;
+            let t = 0;
+            while(i <= middle && j <= end){
+                if(arr1[i] <= arr1[j]){
+                    temp[t++] = arr1[i++];
+                }
+                else{
+                    temp[t++] = arr1[j++];
+                }
+            }
+            
+            while (i <= middle){
+                temp[t++] = arr1[i++];
+            }
+
+            while (j <= end){
+                temp[t++] = arr1[j++];
+            }
+            t = 0;
+            while(first <= end){
+                arr[first++] = temp[t++];
+            }
+        }
+        mergeMain(arr,0,arr.length-1,[]);
+        console.log(arr);
+    }
+
+    //堆排序  数据结构等同完全二叉树
+    heapSort(arr1){
+        let len;
+        //首次建立最大堆
+        function createMaxHeap(arr){
+            len = arr.length;
+            for(let i= Math.floor((len-1)/2);i >=0 ;i --){
+                maxHeapFixdown(arr,i);
+            }
+        }
+
+        function maxHeapFixdown(arr,i){
+            let left = 2 * i + 1;//左子节点
+            let right = 2 * i + 2;//右子节点
+            let largest = i;
+            if (left < len && arr[left] > arr[largest]) {
+                largest = left;
+            }
+
+            if (right < len && arr[right] > arr[largest]) {
+                largest = right;
+            }
+
+            if (largest != i) {
+                swap(arr, i, largest);
+                maxHeapFixdown(arr, largest);
+            }
+        }
+
+        function swap(arr, i, j) {
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        createMaxHeap(arr1);
+        for(let i = arr1.length -1 ;i > 0; i--){
+            swap(arr1, 0, i);//将最大数置于最后
+            len --;//排除最大数后，重新建立最大堆
+            maxHeapFixdown(arr1,0);
+        }
+        console.log(arr1);
+    }
+
+    //计数排序
+    countSort(arr){
+        let bucket = [],sortedIndex = 0,len = arr.length;
+        for(let i = 0;i < len;i++){
+            if(!bucket[arr[i]]){
+                bucket[arr[i]] = 0;
+            }
+            bucket[arr[i]] ++;
+        }
+        console.log(bucket);
+        for(let j = 0;j < bucket.length;j++){
+            while(bucket[j] >0){
+                arr[sortedIndex++] = j;
+                bucket[j] --;
+            }
+        }
+        console.log("计数排序",arr);
+    }
+
     render() {
         return (
             <div className="Login">
                 <button onClick={() => this.doNext()}>Generator执行next</button>
-                <button onClick={() => this.normalTest()}>测试{this.state.name}-{this.state.count}</button>
+                <br/>
+                <button onClick={() => this.bubbleSort([2,1,6,5,8,3,7,9,4])}>冒泡</button>
+                <br/>
+                <button onClick={() => this.selectSort([2,1,6,5,8,3,7,9,4])}>选择排序</button>
+                <br/>
+                <button onClick={() => this.insertSort([2,1,6,5,8,3,7,9,4])}>插入排序</button>
+                <br/>
+                <button onClick={() => this.fastSort([2,1,6,5,8,3,7,9,4])}>快速排序</button>
+                <br/>
+                <button onClick={() => this.shellSort([2,1,6,5,8,3,7,9,4])}>希尔排序</button>
+                <br/>
+                <button onClick={() => this.mergeSort([2,1,6,5,8,3,7,9,4])}>归并排序</button>
+                <br/>
+                <button onClick={() => this.heapSort([2,1,6,5,8,3,7,9,4])}>堆排序</button>
+                <br/>
+                <button onClick={() => this.countSort([2,1,6,5,8,3,7,9,4])}>计数排序</button>
+                <button className="normalTest" ref={e => this.testE = e} onClick={() => this.normalTest()}>测试{this.state.name}-{this.state.count}</button>
                 <div onClick={() => this.props.createChangeContent("Change State redux")}>{this.props.title}</div>
                 <div onClick={() => console.log(this.props)}>打印props</div>
                 <div onClick={() => this.props.httpGet()}>(redux中间件)发送http</div>
